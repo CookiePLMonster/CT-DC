8bdad04dca1dc9966a3aa664e32821573c08222a
Silent <zdanio95@gmail.com>
2021-07-24 19:55:29 +0200

    First commit

---
+++ source/VersionInfo.lua
@@ -0,0 +1,8 @@
+defines { 
+    "rsc_FullName=\"Crazy Taxi Dreamcast Restoration\"",
+    "rsc_MinorVersion=0",
+    "rsc_RevisionID=1",
+    "rsc_BuildID=0",
+    "rsc_Copyright=\"2021\"",
+    "rsc_UpdateURL=\"https://github.com/CookiePLMonster/CT-DC/releases\""
+}
