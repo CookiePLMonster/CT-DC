a996230c1d3a0a54d835da51322e3c4eeeff6be1
Silent <zdanio95@gmail.com>
2021-07-25 13:56:45 +0200

    Restore Pizza Hut and FILA logos and clothes

--- source/CT-DC.cpp
+++ source/CT-DC.cpp
@@ -33,4 +33,56 @@ void OnInitializeHook()
 		nopComparison("81 F9 A0 B3 C0 0C", 2);
 	}
 
+	
+	// Restore Pizza Hut
+	{
+		nopComparison("81 FB 60 DF BF 0C", 2); // Original
+
+		try
+		{
+			auto addr = get_pattern("0F 8F ? ? ? ? 74 6B", 6); // Arcade
+			Nop(addr, 2);
+		}
+		TXN_CATCH();
+	}
+
+	// Restore FILA
+	{
+		// Original
+		try
+		{
+			auto addr = get_pattern("81 FB 60 84 BF 0C", 6 + 6);
+			Nop(addr, 6);
+		}
+		TXN_CATCH();
+
+		nopComparison("81 FB A0 58 C0 0C", 2); // Arcade
+	}
+
+	// Restore clothes inside FILA
+	{
+		// Original
+		try
+		{
+			auto original_addr = get_pattern("81 FB C0 91 BF 0C", 6 + 2);
+			auto original_jmp_target = get_pattern("81 FB 00 F0 BF 0C", -4 - 5);
+			WriteOffsetValue(original_addr, original_jmp_target);
+		}
+		TXN_CATCH();
+
+		// Arcade
+		try
+		{
+			auto arcade_addr = pattern("8B 3D ? ? ? ? 89 4D 94").get_one();
+			auto arcade_jmp_target = get_pattern("81 BD 5C FF FF FF 40 7F 62 0C");
+
+			// mov byte ptr [ebp+var_59], 0
+			// mov edi, 0F0h
+			// jmp 40857Ch
+			Patch(arcade_addr.get<void>(-6), {0xC6, 0x45, 0xA7, 0x00});
+			Patch(arcade_addr.get<void>(-6 + 4), {0xBF, 0xF0, 0x00, 0x00, 0x00});
+			InjectHook(arcade_addr.get<void>(-6 + 4 + 5), arcade_jmp_target, PATCH_JUMP);
+		}
+		TXN_CATCH();
+	}
 }
